82d67e707141cd6b97ec418eb4e867f10ec642c3
Ubuntu <ubuntu@ip-172-31-87-139.ec2.internal>
2021-12-01 17:30:10 +0000

    Saved code

---
+++ Reverse.java
@@ -0,0 +1,43 @@
+/*
+* The "Reverse" program reverses strings
+*
+* @author  Myles Trump
+* @version 1.0
+* @since   2021-12-01
+*/
+
+import java.util.Scanner;
+
+/**
+* This is a program that reverses strings.
+*/
+final class Template {
+
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private Template() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+        // variables
+        final double some;
+
+        final Scanner userInput = new Scanner(System.in);
+
+        if (
+
+        System.out.println("\nDone.");
+    }
+}
